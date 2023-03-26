d3d1b4b18c3bca25a98d7bd69584171f2f0aab84
k-kbot <polykkpodh1243@gmail.com>
2023-03-26 13:48:45 +0900

    つぶやきを編集する処理の作成

--- resources/views/tweet/update.blade.php
+++ resources/views/tweet/update.blade.php
@@ -11,27 +11,22 @@
 <body>
   <h1>つぶやきアプリ</h1>
   <div>
+    <a href="{{ route('tweet.index') }}">戻る</a>
     <p>投稿フォーム</p>
-    <form action="{{ route('tweet.create') }}" method="post">
+    @if (session('feedback.success'))
+    <p style="color: green">{{ session('feedback.success') }}</p>
+    @endif
+    <form action="{{ route('tweet.update.put', ['tweetId' => $tweet->id]) }}" method="post">
+      @method('PUT')
       @csrf
       <label for="tweet-content">つぶやき</label>
       <span>140文字まで</span>
-      <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力"></textarea>
+      <textarea id="tweet-content" type="text" name="tweet" placeholder="つぶやきを入力">{{ $tweet->content }}</textarea>
       @error('tweet')
       <p style="color: red">{{ $message }}</p>
       @enderror
-      <button type="submit">投稿</button>
+      <button type="submit">編集</button>
     </form>
-  </div>
-  <div>
-    @foreach($tweets as $tweet)
-    <details>
-      <summary>{{ $tweet->content }}</summary>
-      <div>
-        <a href="{{ route('tweet.update.index', ['tweetId' => $tweet->id]) }}">編集</a>
-      </div>
-    </details>
-    @endforeach
   </div>
 </body>
 
